f62c07e047accb4d4877bba26b1bab71422dcebf
ast8705 <andtoskas1104@gmail.com>
2024-08-28 19:39:11 -0400

    added text

--- wesbite-portfolio/src/app/app.component.ts
+++ wesbite-portfolio/src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+import {MatChipsModule} from '@angular/material/chips';
+import { ExperiencesComponent } from './experiences/experiences.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent
+  imports: [RouterOutlet, NavbarComponent, MatChipsModule, RouterModule, ExperiencesComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
--- wesbite-portfolio/src/app/app.routes.ts
+++ wesbite-portfolio/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ExperiencesComponent } from './experiences/experiences.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+      path: 'experiences',
+      component: ExperiencesComponent,
+    },
+  ];
+
